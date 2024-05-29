b29e6d7842ce932e20f2f88be16ff68298892bb0
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-05-29 07:56:35 +0530

    20240528

---
+++ java/src/csbca2403/todoapp/TodoItem.java
@@ -0,0 +1,28 @@
+package csbca2403.todoapp;
+
+import java.awt.Frame;
+import java.util.Date;
+
+public class TodoItem extends Frame {
+    int id;
+    String title;
+    String notes;
+    Date dateAdded;
+    Date dueDate;
+
+    void setTodoItem() {
+
+    }
+
+    void getTodoItem() {
+
+    }   
+    
+    void getTodoItems() {
+
+    }
+
+    public static void main(String[] args) {
+        new TodoItem();
+    }
+}
