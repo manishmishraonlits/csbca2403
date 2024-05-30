4693a63cd7e29599ec28f1dc3adec88fee22576f
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-05-30 07:54:01 +0530

    20240529

---
+++ java/src/csbca2403/todoapp/TodoApp.java
@@ -0,0 +1,9 @@
+package csbca2403.todoapp;
+
+public class TodoApp {
+    public static void main(String[] args) {
+        TodoItem todoItem = new TodoItem();     //Class instantiation
+        System.out.println(todoItem);
+        todoItem.setTodoItem();
+    }
+}
--- java/src/csbca2403/todoapp/TodoItem.java
+++ java/src/csbca2403/todoapp/TodoItem.java
@@ -1,17 +1,22 @@
 package csbca2403.todoapp;
 
-import java.awt.Frame;
 import java.util.Date;
 
-public class TodoItem extends Frame {
+public class TodoItem {
     int id;
     String title;
-    String notes;
-    Date dateAdded;
-    Date dueDate;
+    String note;
+    // Date dateAdded;
+    // Date dueDate;
+
+    TodoItem() {
+        id = 1;
+        title = "Complete Java Assignment";
+        note = null;
+    }
 
     void setTodoItem() {
-
+        
     }
 
     void getTodoItem() {
@@ -20,9 +25,5 @@ public class TodoItem extends Frame {
     
     void getTodoItems() {
 
-    }
-
-    public static void main(String[] args) {
-        new TodoItem();
     }
 }
