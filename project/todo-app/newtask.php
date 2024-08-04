15bc8c63289deaf9fecc23784e0c5cfa01cf6bfb
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-08-04 10:27:28 +0530

    20240804

---
+++ project/todo-app/newtask.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Task</title>
+</head>
+<body>
+    <form>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name">
+        <label for="due-date">Due Date</label>
+        <input type="date" name="due-date" id="due-date">
+        <button>Submit</button>
+    </form>
+</body>
+</html>
